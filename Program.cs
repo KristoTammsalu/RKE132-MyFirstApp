e86273e0ddb791f709ec634d0ded79675049dc3d
Kristi Tammsalu <kristi.tammsalu@metalexpress.local>
2023-02-01 19:09:22 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// This is a comment to my code
+
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//sting - sõne
+string Username;
+
+Username = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + Username + "!"); //Output
+//string- interpolation
+
+Console.WriteLine($"Hello, {Username} !"); //Output
+
+
